d6e482f8cc0212ec81762acc7a9416799ca91a7c
Lilyana Zhang <impro4everrr@gmail.com>
2025-09-30 10:22:11 -0400

    Create worst_guessing_game.py

---
+++ 3.3-boolean-operators/worst_guessing_game.py
@@ -0,0 +1,8 @@
+secret_number = 2
+
+user_guess = int(input('TEH WORST NUBMER GESSING GAME EVAR!!!!!!!1!\nI"M THKING OF A NBR FROM 1-10.  TRY 2 GESS! '))
+
+if user_guess == secret_number:
+    print(f"LOL!!! U GOT IT!  I CANT BELEIVE U GESSED IT WAS {secret_number}!")
+else:
+    print(f"W00T!  U SUX0R!!!  I PWN J00!!!  IT WAS {secret_number}!")
