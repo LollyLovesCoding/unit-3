e30d7fae1c708f33e3079aa0b41b432250acc26b
Lilyana Zhang <impro4everrr@gmail.com>
2025-09-30 10:12:22 -0400

    Create alpha_order.py

---
+++ 3.3-boolean-operators/alpha_order.py
@@ -0,0 +1,16 @@
+# First word comes before second word
+print(f"'yes' comes before 'zoo': {'yes' < 'zoo'}")
+print(f"'apple' comes before 'banana': {'apple' < 'banana'}")
+print(f"'zane' comes before 'zune': {'zane' < 'zune'}")
+print(f"'dog' comes before 'hamster': {'dog' < 'hamster'}")
+print(f"'angel' comes before 'lilyana': {'angel' < 'lilyana'}")
+
+# First word comes after the second word
+print(f"'angel' comes after 'Lilyana': {'angel' > 'Lilyana'}")
+print(f"'sad' comes after 'happy': {'sad' > 'happy'}")
+print(f"'egg' comes after 'chicken': {'egg' > 'chicken'}")
+print(f"'dog' comes after 'bone': {'dog' > 'bone'}")
+print(f"'Samsung' comes after 'Apple': {'Samsung' > 'Apple'}")
+
+# Why does "Dog" come before "axe"?
+# "Dog" comes before "axe" because the "D" is capitalized, to fix this error, either lower the "D" or capitalize the "a" in "axe".
