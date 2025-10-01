779d5f0ed29278f920301a0eaf377fc5204d5914
Lilyana Zhang <impro4everrr@gmail.com>
2025-10-01 09:58:44 -0400

    Create dice.py

---
+++ 3.4-RNG/dice.py
@@ -0,0 +1,6 @@
+import random
+roll_1 = random.randrange(1, 7)
+roll_2 = random.randrange(1, 7)
+print("HERE COMES THE DICE!\n")
+print(f"Roll #1: {roll_1}\nRoll #2: {roll_2}\nThe total is {roll_1 + roll_2}!")
+
